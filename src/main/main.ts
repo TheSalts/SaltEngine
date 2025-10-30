f1f68812e43b4f18f66d00baf5d1deae1b765f5c
TheSalt_ <joon_sa@naver.com>
2025-10-30 20:27:32 +0900

    first commit

---
+++ src/main/main.ts
@@ -0,0 +1,41 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+/**
+ * Electron 메인 프로세스 진입점
+ */
+function createWindow(): void {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, '../preload/preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
